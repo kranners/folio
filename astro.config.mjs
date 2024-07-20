fa1b1a8e762e368f5f5fd3afd6d8f417288c7d6a
Aaron Pierce <aaron@cute.engineer>
2024-07-20 16:22:02 +1000

    build(astro): installing frameworks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), react(), svelte(), vue()]
+});
